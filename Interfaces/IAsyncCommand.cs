994256a253d6227540a62ebb998e52939537618f
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-12 12:25:09 +0100

    Add new command parser (#1)
    * Add new command parser
    
    * Removed IAsyncCommand as its no longer needed
    
    * update version
    
    * Remove unused directives

--- Interfaces/IAsyncCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using AltV.Net.Elements.Entities;
-
-namespace AltV.Icarus.Commands.Interfaces;
-
-public interface IAsyncCommand : ICommandData
-{
-    public Task OnCommandAsync( IPlayer player, string[ ] args );
-}
--- Interfaces/ICommandData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AltV.Icarus.Commands.Interfaces;
-
-public interface ICommandData
-{
-    public string Name { get; set; }
-    public string[ ] Aliases { get; set; }
-    public string Description { get; set; }
-    public uint RequiredLevel { get; set; }
-}
