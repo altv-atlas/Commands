e92292fd5b48e7cf2109d331bcf1fe17c71a1dfc
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-12 12:25:08 +0100

    Initial setup of command module

---
+++ CommandModule.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+using AltV.Icarus.Commands.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AltV.Icarus.Commands;
+
+public static class CommandModule
+{
+    private static readonly ICollection<Type> Commands = new List<Type>( );
+    internal static string EventName = "chat:message";
+    
+    public static IServiceCollection RegisterCommandModule( this IServiceCollection services, string eventName = "chat:message" )
+    {
+        EventName = eventName;
+        
+        services.AddSingleton<CommandManager>( );
+        
+        RegisterCommandType( services, typeof( IAsyncCommand ) );
+        RegisterCommandType( services, typeof( ICommand ) );
+        
+        return services;
+    }
+
+    private static void RegisterCommandType( IServiceCollection services, Type type )
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies( );
+        
+        var commands = assemblies
+            .SelectMany( s => s.GetTypes( ) )
+            .Where( p => type.IsAssignableFrom( p ) && p.IsClass );
+
+        foreach( var command in commands )
+        {
+            Commands.Add( command );
+            services.AddSingleton( command );
+        }
+    }
+    
+    public static IServiceProvider InitializeCommandModule( this IServiceProvider serviceProvider )
+    {
+        var count = 0;
+
+        var commandManager = serviceProvider.GetService<CommandManager>( );
+
+        var logger = serviceProvider.GetService<ILogger<Logger>>( );
+        
+        if( commandManager is null || logger is null )
+            throw new NullReferenceException( "Failed to initialize CommandManager" );
+        
+        foreach( var type in Commands )
+        {
+            // Console.WriteLine( $"[{ModuleName}] Instantiating command: { type.Name }" );
+            var command = serviceProvider.GetService( type );
+
+            if( command is null )
+                continue;
+            
+            commandManager.RegisterCommand( type, command );
+            count++;
+        }
+
+        logger.LogInformation( "{Count} commands initialized!", count );
+        return serviceProvider;
+    }
+    
+    private class Logger {}
+}
---
+++ Interfaces/IAsyncCommand.cs
@@ -0,0 +1,8 @@
+﻿using AltV.Net.Elements.Entities;
+
+namespace AltV.Icarus.Commands.Interfaces;
+
+public interface IAsyncCommand : ICommandData
+{
+    public Task OnCommandAsync( IPlayer player, string[ ] args );
+}
